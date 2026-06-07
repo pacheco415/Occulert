aa938b53cb52a59e9448ea4a47f299d6cbcd0c33
pacheco415 <pacheco415@yahoo.com>
2026-06-07 11:55:42 -0700

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const CACHE = 'occulert-v1';
-const ASSETS = ['/', '/app.html', '/manifest.json'];
+const CACHE = 'occulert-v2';
+const ASSETS = ['/', '/index.html', '/app.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+    e.waitUntil(caches.keys().then(keys =>
+          Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+                                     ));
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/')))
-  );
+    e.respondWith(
+          caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/')))
+        );
 });
