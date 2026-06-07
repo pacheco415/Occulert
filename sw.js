37acf9c7988430599aed61cc82392e8201a7d8fc
pacheco415 <pacheco415@yahoo.com>
2026-06-07 12:33:30 -0700

    Upgrade service worker cache version to v3

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-const CACHE = 'occulert-v2';
+const CACHE = 'occulert-v3';
 const ASSETS = ['/', '/index.html', '/app.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-    self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys =>
-          Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-                                     ));
-    self.clients.claim();
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  // Network-first for HTML so updates always get through
+  if (e.request.mode === 'navigate') {
     e.respondWith(
-          caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/')))
-        );
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/')))
+  );
 });
